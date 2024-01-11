d705acb2c4f70bf54b2d5f3b735edc00e142ee54
Guilherme Inacio <gui-inacio-2010@hotmail.com>
2024-01-10 21:04:00 -0300

    Reset Password Controller

---
+++ src/modules/users/controllers/ResetPasswordController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import ResetPasswordService from "../services/ResetPasswordService";
+
+
+export default class ResetPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { password, token } = request.body;
+
+    const resetPassowrd = new ResetPasswordService();
+
+    await resetPassowrd.execute({
+      password,
+      token,
+    });
+
+    return response.status(204).json();
+  }
+}
